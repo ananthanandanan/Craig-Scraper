2bfb9ac70e9307835de014793a420ed6964eb4ff
ananthanandanan <ananthanandanan@gmail.com>
2020-10-29 23:39:00 +0530

    added the base template and Search model

---
+++ my_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Search(models.Model):
+
+    search = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return '{}'.format(self.search)
+    
+    
+    class Meta:
+        
+        verbose_name_plural = 'Searches'
+
