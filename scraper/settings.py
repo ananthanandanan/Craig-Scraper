a2ea5a9543ed17ac07a368956c34807225c25679
ananthanandanan <ananthanandanan@gmail.com>
2020-11-03 01:05:54 +0530

    add meta class

--- scraper/settings.py
+++ scraper/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
